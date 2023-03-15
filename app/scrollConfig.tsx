8b9983624b09270f4be5c510a4a183280fe13323
Dustin Hyden <dustinbhyden@gmail.com>
2023-03-15 15:07:42 -0400

    Added max number of homepage slides

--- app/scrollConfig.tsx
+++ app/scrollConfig.tsx
@@ -1,13 +1,18 @@
 import data from "./data"
 
 export const scrollConfig = {
+  maxNumberOfSlides: 5,
   offsetFromPlayer: 8, // how far off the screen should the projects be from the player track.
   spacing: 40, // distance between objects
   floorPos: -4.5, // how much do we need to adjust the height to shrink the objects
   scrollSpeed: 0.025, // how quickly to scroll the projects
   showHideDepth: 0.35,
   totalScrollHeight: () =>
-    (scrollConfig.spacing * (data.length - 1)) / scrollConfig.scrollSpeed,
+    (scrollConfig.spacing *
+      (data.length >= scrollConfig.maxNumberOfSlides
+        ? scrollConfig.maxNumberOfSlides - 1
+        : data.length - 1)) /
+    scrollConfig.scrollSpeed,
   pixelsPerProject: () => scrollConfig.spacing / scrollConfig.scrollSpeed,
 }
 
