7b5cb23a020a993f6770b4259e6846a8375f62f8
Dustin Hyden <dustinbhyden@gmail.com>
2023-03-30 09:18:05 -0400

    testing autoplay fixes

--- app/components/Video.tsx
+++ app/components/Video.tsx
@@ -1,3 +1,11 @@
 export default function Video({ src }: { src: string }) {
-  return <video autoPlay loop muted src={src}></video>
+  return (
+    <video
+      src={src}
+      autoPlay={true}
+      loop={true}
+      muted={true}
+      playsInline={true}
+    />
+  )
 }
