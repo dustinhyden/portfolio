3cc9fe375ac07c5f6b77a6bfadc52b453665232c
Dustin Hyden <dustinbhyden@gmail.com>
2023-03-30 11:59:23 -0400

    finished initial page loader

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -2,33 +2,36 @@ import Image from "next/image"
 import { useEffect, useState } from "react"
 import JumboButton from "./JumboButton"
 import styles from "./Loader.module.css"
+import Video from "./Video"
+
 export default function Loader() {
   const [loaded, setLoaded] = useState(false)
   const duration = 3
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      console.log("finished")
       setLoaded(true)
+      document.querySelector("body").classList.remove("no-scroll")
     }, duration * 1000)
 
     return () => clearTimeout(timer)
   }, [])
 
-  return <div>hi</div>
+  // return <div>hi</div>
   return (
     <div className={`${styles.loader} ${loaded ? styles.done : ""}`}>
       <div className={styles["container"]}>
-        <Image
+        <Video src="/loading-anim.webm" />
+        {/* <Image
           src="/loader.png"
           alt="Cartoon head loading image"
           width={150}
           height={150}
-        />
+        /> */}
         <p className={styles["loading-text"]}>
           {loaded ? "Done!" : "Loading..."}
         </p>
-        <section className={styles["loading-container"]}>
+        <section className={styles["effect"]}>
           <Image
             className={styles["loading-bar"]}
             src="/loading-bar.svg"
