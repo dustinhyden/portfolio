ea0cf7c665495796be70b513d9d272654dd116dd
Dustin Hyden <dustinbhyden@gmail.com>
2023-04-12 11:43:02 -0400

    finished styling highlight component

--- app/components/Highlight.tsx
+++ app/components/Highlight.tsx
@@ -1,13 +1,21 @@
 "use client"
-import SyntaxHighlighter from 'react-syntax-highlighter';
+import SyntaxHighlighter from "react-syntax-highlighter"
 // import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomOneLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import {
+  atomOneLight,
+  tomorrowNight,
+} from "react-syntax-highlighter/dist/esm/styles/hljs"
 import styles from "./Highlight.module.css"
 
-export default function Highlight({content, language = "javascript"}) {
-
+export default function Highlight({ content, language = "javascript" }) {
   return (
-    <SyntaxHighlighter language={language} style={atomOneLight} className={styles.highlight} showLineNumbers={true} wrapLines={true}>
+    <SyntaxHighlighter
+      language={language}
+      style={tomorrowNight}
+      className={styles.highlight}
+      showLineNumbers={true}
+      wrapLines={true}
+    >
       {content}
     </SyntaxHighlighter>
   )
