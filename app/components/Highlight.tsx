50125d1228f2b5cd3088cb73238db5c20686188e
Dustin Hyden <dustinbhyden@gmail.com>
2023-04-12 14:51:13 -0400

    added behind-the-scenes

--- app/components/Highlight.tsx
+++ app/components/Highlight.tsx
@@ -7,7 +7,11 @@ import {
 } from "react-syntax-highlighter/dist/esm/styles/hljs"
 import styles from "./Highlight.module.css"
 
-export default function Highlight({ content, language = "javascript" }) {
+export default function Highlight({
+  content,
+  language = "javascript",
+  startingLineNumber = 1,
+}) {
   return (
     <SyntaxHighlighter
       language={language}
@@ -15,6 +19,7 @@ export default function Highlight({ content, language = "javascript" }) {
       className={styles.highlight}
       showLineNumbers={true}
       wrapLines={true}
+      startingLineNumber={startingLineNumber}
     >
       {content}
     </SyntaxHighlighter>
